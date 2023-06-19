c60cbef0fe03036512e0b96ad859fe3588e9e437
Vũ Văn Đạt <datvv942@gmail.com>
2023-06-19 14:51:40 +0700

    config file

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,7 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import './assets/styles/app.scss'
+import 'ant-design-vue/dist/antd.less'
 
 createApp(App).use(router).mount('#app')
--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,21 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue({})],
+  css: {
+    preprocessorOptions: {
+      less: {
+        modifyVars: {
+          'primary-color': '#00904a',
+        },
+        javascriptEnabled: true,
+      },
+      scss: {
+        additionalData: `
+            @import "./src/assets/styles/common/_variable.scss";
+          `,
+      },
+    },
+  },
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
