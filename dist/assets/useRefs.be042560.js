d605c04d75fce896295e62b594bf750d3d346520
Vũ Văn Đạt <datvv942@gmail.com>
2023-06-19 13:38:47 +0700

    abc

--- dist/assets/useRefs.be042560.js
+++
@@ -1,1 +0,0 @@
-import{a as n,D as s}from"./index.118c7d70.js";var t=function(){var e=n(new Map),r=function(a){return function(f){e.value.set(a,f)}};return s(function(){e.value=new Map}),[r,e]},i=t;export{i as u};
--- src/router/index.ts
+++ src/router/index.ts
@@ -2,7 +2,6 @@ import { createRouter, createWebHistory } from "vue-router";
 
 import routes from "./routes";
 import MainLayout from "@/shared/layout/MainLayout/index.vue";
-import { RouteName } from "@/shared/constants";
 
 const router = createRouter({
   history: createWebHistory(),
@@ -13,22 +12,6 @@ const router = createRouter({
       children: routes,
     },
   ],
-});
-
-router.beforeEach((to, from, next) => {
-  const accessToken = localStorage.getItem(
-    import.meta.env.VITE_ACCESS_TOKEN_NAME
-  );
-
-  if (!to.meta.auth) {
-    return next();
-  }
-
-  if (!accessToken) {
-    return next({ name: RouteName.HOMEPAGE });
-  }
-
-  return next();
 });
 
 export default router;
